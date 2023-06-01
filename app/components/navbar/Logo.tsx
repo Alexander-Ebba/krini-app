fbc18823982687f5bff143cf0ba5b42c7dc5e189
XorWain <seigneurdofus931@gmail.com>
2023-06-01 00:12:27 +0000

    first commit

---
+++ app/components/navbar/Logo.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import Image from 'next/image';
+import { useRouter } from "next/navigation";
+
+const Logo = () => {
+    const router = useRouter();
+
+    return (
+        <Image 
+            onClick={() => router.push('/')}
+            alt='logo'
+            className='hidden md:block cursor-pointer'
+            height="150"
+            width="150"
+            src='/images/logo.png'
+        />
+    )
+}
+
+export default Logo;
